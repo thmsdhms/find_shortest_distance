ec7653ae811cd3063bb1b262f45d72dc39c82e61
Thomas Dahms <dahms@live.com>
2018-09-14 18:39:11 +0200

    Implement find_shortest_distance.
    Implements a function that returns the shortest distance of two
    substrings in a string. Distance is considered the number of words
    between the substrings.

---
+++ find_shortest_distance.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+def find_shortest_distance(a, b, s):
+    """
+    Finds the shortest distance between substrings a and b in s. Distance is expressed as number of words between the substrings.
+
+    For example:
+    >>> find_shortest_distance('motivation', 'development', 'We do value and reward motivation in our development team. Development is a key skill for a DevOp.')
+    2
+
+    It does not matter which of the two arguments comes first:
+    >>> find_shortest_distance('motivation', 'development', 'We do value and reward development in our motivation team. Motivation is a key skill for a DevOp.')
+    2
+
+    If at least one of the arguments cannot be found, this is indicated by a result None:
+    >>> find_shortest_distance('foo', 'bar', '') is None
+    True
+
+    Distance between identical arguments expects multiple occurences:
+    >>> find_shortest_distance('foo', 'foo', 'foo') is None
+    True
+    >>> find_shortest_distance('foo', 'foo', 'foo foo')
+    0
+
+    Adjacent punctuation is considered part of a word:
+    >>> find_shortest_distance('foo', 'bar', 'foo. between bar') is None
+    True
+    >>> find_shortest_distance('foo', 'bar', 'foo . between bar')
+    2
+
+    Words can be split by newlines, tabs or multiple spaces, too:
+    >>> find_shortest_distance('foo', 'bar', 'foo\\nbetween\\tbetween  bar')
+    2
+
+    Multibyte characters work as well:
+    >>> find_shortest_distance('\U0001f604isAnEmoji', 'foo', 'foo bar \U0001f604isAnEmoji')
+    1
+
+    Words appearing as parts of other words are not a match:
+    >>> find_shortest_distance('foo', 'bar', 'foobaz bar') is None
+    True
+
+    The shortest distance need not be the first distance:
+    >>> find_shortest_distance('foo', 'bar', 'foo word bar foo')
+    0
+    """
+    words = s.split()
+    a_indices = [ i for i, word in enumerate(words) if word == a ]
+    b_indices = [ i for i, word in enumerate(words) if word == b ]
+    distances = [ abs(x - y) - 1 for x in a_indices for y in b_indices if abs(x - y) > 0 ]
+    return min(distances) if distances else None
